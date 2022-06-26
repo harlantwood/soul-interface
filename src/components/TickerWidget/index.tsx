da2b4af92939d368ee2a87655577254ea7c17c50
Buns <bunsthedev@gmail.com>
2022-06-26 02:32:51 -0500

    try-add: home (wip)

--- src/components/TickerWidget/index.tsx
+++ src/components/TickerWidget/index.tsx
@@ -3,7 +3,6 @@ export const TickerWidget = () => {
     
     <><script src="https://crypto.com/price/static/widget/index.js"></script><div
         id="crypto-widget-CoinMarquee"
-
         data-theme="dark"
         data-design="classic"
         data-coin-ids="166,5876,2180,9251"></div></>
---
+++ src/pages/stats/index.tsx
@@ -0,0 +1,9 @@
+import TickerWidget from "components/TickerWidget";
+import React from "react";
+
+const Stats = () => {
+
+return (<TickerWidget/>)
+  }
+
+export default Stats
