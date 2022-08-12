19f0f937a83d732d923f37ab1c87150191e1b496
Buns <68980965+BunsDev@users.noreply.github.com>
2022-08-12 18:21:53 -0500

    crossy

---
+++ src/pages/crossy/index.tsx
@@ -0,0 +1,15 @@
+// import Container from "components/Container";
+import React from "react";
+
+const Lifi = () => {
+	return (
+      <iframe 
+			frameBorder={"none"}
+    		title={"Lifi"}
+    		src="https://lifi-hrnnd0mxu-bunsdev.vercel.app"
+    		height={"900px"}
+    		width={"100%"}
+    />
+	);
+};
+export default Lifi;
