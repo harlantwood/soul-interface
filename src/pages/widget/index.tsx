c5f16c9ba63af5ddab48ec317bf7abf148ea6795
Buns <bunsthedev@gmail.com>
2022-08-13 00:07:59 -0500

    alternative attempt: widget

--- src/pages/widget/index.tsx
+++ src/pages/widget/index.tsx
@@ -411,20 +411,20 @@ const App = () => {
   );
 };
 
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/swap/*" element={<App />} />
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/widget/*" element={<App />} />
+//         <Route path="/*" element={<App />} />
+//       </Routes>
+//     </BrowserRouter>
+//   </React.StrictMode>,
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-if (process.env.NODE_ENV === 'development') {
+// if (process.env.NODE_ENV === 'development') {
 //   reportWebVitals(console.log);
-}
+// }
